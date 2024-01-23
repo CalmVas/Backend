f5a779c8838c72e8eba83526fdc652490781e3db
sunwupark <sunwu5678@gmail.com>
2024-01-23 22:30:56 +0900

    Feat: Controller (Auth, UseR)

--- KhuT/src/main/java/prometheus/KhuT/Controller/UserController.java
+++ KhuT/src/main/java/prometheus/KhuT/Controller/UserController.java
@@ -1,28 +1,47 @@
 package prometheus.KhuT.Controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import prometheus.KhuT.Common.BaseResponseDto;
 import prometheus.KhuT.Domain.User;
+import prometheus.KhuT.Dto.User.UserDto;
 import prometheus.KhuT.Dto.User.UserRequest;
 import prometheus.KhuT.Dto.User.UserResponse;
 import prometheus.KhuT.Service.UserService;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
+@RequestMapping("/api")
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/api/v1/login")
+    @PostMapping("/v1/login")
     public BaseResponseDto<UserResponse.UserCreateResponse> login(@RequestBody UserRequest.UserCreateRequest userCreateRequest){
         System.out.println("userCreateRequest. = " + userCreateRequest.getName());
         UserResponse.UserCreateResponse joined = userService.join(userCreateRequest);
         return BaseResponseDto.onSuccess(joined);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(
+            @Valid @RequestBody UserDto userDto
+    ) {
+        return ResponseEntity.ok(userService.signup(userDto));
+    }
+
+    @GetMapping("/user")
+//    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public ResponseEntity<User> getMyUserInfo() {
+        return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
+    }
+
+    @GetMapping("/user/{username}")
+//    @PreAuthorize("hasAnyRole('ADMIN')")
+    public ResponseEntity<User> getUserInfo(@PathVariable String username) {
+        return ResponseEntity.ok(userService.getUserWithAuthorities(username).get());
     }
 
 
