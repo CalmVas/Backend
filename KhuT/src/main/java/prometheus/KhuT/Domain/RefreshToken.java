c9116e85660246c0a9f1d08a08ae01d9c0d0ded0
sunwupark <sunwu5678@gmail.com>
2024-01-23 22:31:28 +0900

    Feat: Token and Redis Components

---
+++ KhuT/src/main/java/prometheus/KhuT/Domain/RefreshToken.java
@@ -0,0 +1,26 @@
+package prometheus.KhuT.Domain;
+
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+//@RedisHash(value = "refreshToken", timeToLive = 60)
+public class RefreshToken implements Serializable {
+
+    @Id
+    private String refreshToken;
+    private Long memberId;
+
+    public RefreshToken(final String refreshToken, final Long memberId) {
+        this.refreshToken = refreshToken;
+        this.memberId = memberId;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+}
